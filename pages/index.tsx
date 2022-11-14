bbefccfabf7e4a65a5ca520a99c3cc079b3f4a05
Matthew Cook <matthewcook13987@gmail.com>
2022-11-13 18:52:29 -0600

    adds rating star component and css changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import axios from "axios";
+import styles from '../styles/Home.module.css'
 import React, { useState, useEffect } from "react";
 import { json } from "stream/consumers";
+import { RatingStar } from "rating-star";
 
-const url1 = "https://dummyjson.com/products";
+
+
+const url1 = "https://dummyjson.com/products?skip=5&limit=100";
 const url2 = "https://jsonplaceholder.typicode.com/posts"
 const url3 = "http://localhost:3000/api/random-num"
 
@@ -10,7 +14,12 @@ export default function Home() {
 
   const [data, setData] = useState([]);
   const [num, setNum] = useState([]);
-  // let num = "";
+
+  const [clicked, setClicked] = React.useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
 
   const apiGet = () => {
     fetch(url1)
@@ -32,34 +41,49 @@ export default function Home() {
   };
 
   const imgStyle = {
-      width: "250px",
+      width: "150px",
+      height: "150px"
   };
 
-  return (
-    <>
-      <div>
-        <h1>API Call</h1>
-        <h2>Your number is {num}</h2>
-        <button onClick={numberGet}>get random num</button>
-        <br />
-          <button onClick={apiGet}>Fetch API</button>
-        <br />
-          <ul>
-              {data.map(({id, title, description, price, stock, brand, category, thumbnail, images, rating}: any) => (
-                <li key={id}>
-                  {title}
-                  <img style={imgStyle} src={thumbnail}/>
-                  <p>{description}</p>
-                  <p>${price}</p>
-                  <p>rating:{rating}</p>
-                  <p>{stock} in stock</p>
-                  <p>made by {brand}</p>
-                  <p>{category}</p>
-                </li>
-              ))}
-          </ul>
+  const renderedData = data.map(({id, title, description, price, stock, brand, category, thumbnail, images, rating}: any) => (
+      <div key={id} className={styles.card}>
+        <h2>{title}</h2>
+        <img style={imgStyle} src={thumbnail}/>
+        <p>{description}</p>
+        <sup>${price}</sup>
+          <p>
+            <RatingStar id={id} rating={rating} /><sup>{rating}</sup>
+          </p>
+        <p>Only {stock} left in stock!</p>
+        <h3>{brand}</h3>
       </div>
-    </>
+  ));
+
+  return (
+    <div className={styles.container}>
+        <main className={styles.main}>
+
+              <h1 className={styles.title}>Product Viewer</h1>
+
+              {/* <div className={styles.description}>
+                {num && <p>Your number is {num}</p>}<button onClick={numberGet}>get random num</button>
+              </div> */}
+
+              <div className={styles.description}>
+                <button onClick={apiGet}>Fetch API</button>
+                {data.length > 1 ? <p>data</p> : <p>no data</p>}
+                {clicked ? <button onClick={handleClick}>View</button> : <button onClick={handleClick}>Hide</button>}
+              </div>
+
+              <div className={!clicked? styles.gridshow : styles.gridhide}>
+                {renderedData}
+              </div>
+
+
+              <div>
+            </div>
+        </main>
+    </div>
   )
 
 
